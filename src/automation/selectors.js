3258c7bf47d055dfff22aed27c43267adb4137e6
hesher116 <georgsbox116@gmail.com>
2026-05-07 02:55:50 +0200

    Fix checkout text selectors

--- src/automation/selectors.js
+++ src/automation/selectors.js
@@ -7,6 +7,6 @@ module.exports = {
   otpSubmitButton: 'button:has-text("Submit"), text="Submit", div:has-text("Submit")',
   freeRewardLabel: 'div:text-is("Free")',
   rewardImage: 'img[src*="webstore_"], img[src]',
-  orderSummary: 'text="ORDER SUMMARY", text="Order Summary"',
-  claimButton: 'button:has-text("Claim"), [role="button"]:has-text("Claim"), div:text-is("Claim"), text="Claim"'
+  orderSummary: ':text("ORDER SUMMARY"), :text("Order Summary")',
+  claimButton: 'button:text-is("Claim"), [role="button"]:text-is("Claim"), :text-is("Claim")'
 };
